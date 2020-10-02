8129c5b55478226b8245c79340591b46fdc1fb26
troyAmlee <troyamlee19@gmail.com>
2020-10-02 11:35:00 -0500

    [stretch] boofarbaz it works yay tests pass

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,9 +7,18 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
-
+    result = []
+    d = {}
+    for i in files:
+        sp = i.split("/")
+        if(sp[len(sp) - 1] not in d):
+            d[sp[len(sp) - 1]] = [i]
+        elif(sp[len(sp) - 1] in d):
+            d[sp[len(sp) - 1]].append(i)
+    for j in queries:
+        if(j in d):
+            result.extend(d[j])
     return result
-
 
 if __name__ == "__main__":
     files = [
